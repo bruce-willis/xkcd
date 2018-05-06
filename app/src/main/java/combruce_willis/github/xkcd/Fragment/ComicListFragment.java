3a132f4cb645d21fc6fbcaade819e0195760c6cf
Yury Belousov <yury-belousov@yandex.ru>
2018-05-07 01:47:20 +0300

    save position on changing orientation

--- app/src/main/java/combruce_willis/github/xkcd/Fragment/ComicListFragment.java
+++ app/src/main/java/combruce_willis/github/xkcd/Fragment/ComicListFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.SharedElementCallback;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
@@ -81,6 +82,13 @@ public class ComicListFragment extends Fragment {
                 }
             }
         });
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        GridLayoutManager layoutManager = ((GridLayoutManager)recyclerView.getLayoutManager());
+        MainActivity.currentPosition = layoutManager.findFirstCompletelyVisibleItemPosition();
+        super.onSaveInstanceState(outState);
     }
 
     /**
